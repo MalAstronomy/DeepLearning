32f448aef4ad8743ff18e29d6b31e48a96aed3f7
Maxime Quesnel <maxime.quesnel@uliege.be>
2020-05-22 01:14:58 +0200

    Modified ReLU in output

--- training/methods/CNN.py
+++ training/methods/CNN.py
@@ -1,3 +1,4 @@
+import torch 
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -16,20 +17,16 @@ class Net(nn.Module):
         self.fc = nn.Sequential(
             nn.Dropout(),
             nn.Linear(128*4**3, 128),
-            #nn.SELU(),
             nn.ReLU(),
             nn.Dropout(),
             nn.Linear(128, 128),
-            #nn.SELU(),
             nn.ReLU(),
             nn.Dropout(),
             nn.Linear(128, 128),
-            #nn.SELU(),
             nn.ReLU(),
             nn.Dropout(),
             nn.Linear(128, n_channels_out),
-            #nn.SELU())
-            nn.ReLU())    
+            nn.ReLU())
         
     def ConvLayer(self, nb_neurons_in, nb_neurons_out, ksize_conv=3, strd_conv=1, pad_conv=0, ksize_pool=3, strd_pool=1, pad_pool=0):
         '''
@@ -40,23 +37,18 @@ class Net(nn.Module):
                       kernel_size=ksize_conv, stride=strd_conv, padding=pad_conv),
             #nn.BatchNorm3d(nb_neurons_out),
             nn.ReLU(),
-            #nn.SELU(),
             nn.MaxPool3d(kernel_size=ksize_pool, stride=strd_pool, padding=pad_pool))
         return layer
 
-    def forward(self, x):
-        #print("input:",x.size())
-        out = self.layer1(x)
-        #print("1st layer", out.size())
-        out = self.layer2(out)
-        #print("2nd layer", out.size())
-        out = self.layer3(out)
-        #print("3rd layer", out.size())
-        #out = self.layer4(out)
-        #print("4th layer", out.size())
-        out = out.view(out.size(0), -1)  # Flatten for fully connected layers
-        #print("view", out.size())
-        out = self.fc(out)
-        #print("FC", out.size())
+    def forward(self, input):
+        x = self.layer1(input)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        
+        x = x.view(x.size(0), -1)  # Flatten for fully connected layers
+        x = self.fc(x)
+        
+        x = F.relu(x)
+        out = torch.min(x,torch.ones(x.size())) 
         
         return out
--- training/methods/MLP.py
+++ training/methods/MLP.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -9,27 +10,33 @@ class Net(nn.Module):
         self.layer_h1 = self.layer(828, 828)
         self.layer_h2 = self.layer(828, 828)
         self.layer_h3 = self.layer(828, 828)
-        self.layer_out = self.layer(828, n_channels_out, DO_prob=0) #no Dropout applied
+        self.layer_out = self.layer(828, n_channels_out, activation=False, dropout=False) #no activation and Dropout applied
 
         
-    def layer(self, nb_neurons_in, nb_neurons_out, DO_prob=0.5):
-        layer = nn.Sequential(
-            nn.Linear(nb_neurons_in, nb_neurons_out, bias=True),
-            nn.ReLU(),
-            #nn.SELU(),
-            nn.Dropout(p=DO_prob))
+    def layer(self, nb_neurons_in, nb_neurons_out, activation=True, dropout=True):
+        modules = []
+        modules.append(nn.Linear(nb_neurons_in, nb_neurons_out, bias=True))
+        if activation:
+            modules.append(nn.ReLU())
+        if dropout:    
+            modules.append(nn.Dropout())
+
+        layer = nn.Sequential(*modules)
+
         return layer
         
 
-    def forward(self, xb):
-        x = xb.view(xb.size(0), -1) #Flatten data
+    def forward(self, input):
+        x = input.view(input.size(0), -1) #Flatten data
 
         #print(x.size())
         x = self.layer_in(x)
         #print(x.size())
         x = self.layer_h1(x)
         x = self.layer_h2(x)
-        #x = self.layer_h3(x)
-        r = self.layer_out(x)
-
-        return r
+        x = self.layer_out(x)
+        
+        x = F.relu(x)
+        out = torch.min(x,torch.ones(x.size())) # Modified ReLU that outputs 1 for inputs above 1.
+        
+        return out
